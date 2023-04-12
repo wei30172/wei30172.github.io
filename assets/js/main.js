4fb55d7b369f0f3e7119926a85553890503277eb
claireWei <claire610323@gmail.com>
2023-04-12 15:37:10 -0700

    header & nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,32 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+      navClose = document.getElementById("nav-close"),
+      navToggle = document.getElementById("nav-toggle")
+      
 
+// MENU SHOW
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu")
+  })
+}
+
+// MENU CLOSE
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu")
+  })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link")
 
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu")
+  navMenu.classList.remove("show-menu")
+}
+
+navLink.forEach(link => link.addEventListener("click", linkAction))
 
 /*=============== SWIPER PROJECTS ===============*/
 
