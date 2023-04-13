6e3c3568fa61dbb399d430067ef8d3d336ec55eb
claireWei <claire610323@gmail.com>
2023-04-13 16:18:07 -0700

    scroll up & theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,40 @@ const linkAction = () => {
 
 navLink.forEach(link => link.addEventListener("click", linkAction))
 
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up")
+  this.scrollY >= 350 ? scrollUp.classList.add("show-scroll")
+                      : scrollUp.classList.remove("show-scroll")
+}
+
+window.addEventListener("scroll", scrollUp)
+
 /*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById("theme-button")
+const darkTheme = "dark-theme"
+const iconTheme = "ri-sun-line"
+
+const selectedTheme = localStorage.getItem("selected-theme")
+const selectedIcon = localStorage.getItem("selected-icon")
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? "dark" : "light"
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line"
 
 
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme)
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](iconTheme)
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+  localStorage.setItem("selected-theme", getCurrentTheme)
+  localStorage.setItem("selected-icon", getCurrentIcon)
+})
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
